1239b1e63dfbf492e526c61f4af273ff49308b92
Kaykay <kushagrakaushik1620@gmail.com>
2024-10-29 17:26:59 +0530

    fix homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const {data:session, status} = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push(`/dashboard/${session.user.username}`);
+    } else {
+      router.push(`/landing`);
+    }
+  }, [session, status, router]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       HOME
