e08e2fd012c379289552d4b501d5d58f9ae6c718
Kaykay <kushagrakaushik1620@gmail.com>
2025-01-10 00:18:18 +0530

    cancel existing profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -25,34 +25,6 @@ export default function Profile({ params }: any) {
   return (
     <>
       <Topbar />
-      <div className="profile-container mx-auto flex mt-5">
-        <div className="profile-left my-auto">
-          <div className="profile-picture-container mx-auto mt-5">
-            <img src={session?.user.imgUrl} alt="" />
-          </div>
-          <div className="left-button-list mx-auto mt-2 flex flex-col items-center">
-            {/* <ProfilePictureDialog /> */}
-          </div>
-        </div>
-        <div className="profile-right my-auto">
-          <div className="right-header flex justify-between p-5">
-            <div className="details-title">Profile details</div>
-            <div className="edit-profile-button">Edit profile</div>
-          </div>
-          <div className="right-content mx-auto flex flex-col">
-            <div className="content-r1 p-5">
-              <div className="profile-input-group flex">
-                <label htmlFor="">First Name&nbsp;&nbsp;</label>
-                <input type="text" className="w-40" />
-              </div>
-            </div>
-            <div className="content-r2"></div>
-          </div>
-        </div>
-      </div>
-      <div className="profile-analysis-container mx-auto mt-5 flex items-center justify-center">
-        PROFILE ANALYSIS COMING SOON
-      </div>
     </>
   );
 }
